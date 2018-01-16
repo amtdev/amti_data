5a2eaf1a465038862eee844233db3ec4d1407a80
AMTdev Alien <craig@amtdev.com>
2018-01-16 15:08:37 -0500

    added default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,11 @@
-default['dbconf']['socket'] = 'null'
+#db1
+default["dbconf"]["database"] = "lamp_server"
+default["dbconf"]["db_username"] = "lamp_server"
+default["dbconf"]["db_password"] = "88888888"
+default["dbconf"]["socket"] = "/var/run/mysqld/mysqld.sock"
+
+#db2
+default["dbconf2"]["database"] = "null"
+default["dbconf2"]["db_username"] = "null"
+default["dbconf2"]["db_password"] = "null"
+default["dbconf2"]["socket"] = "/var/run/mysqld/mysqld.sock"
