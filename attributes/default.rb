bb42fb2f233b34c1a6aba614c359c0cd04375710
AMTdev Alien <craig@amtdev.com>
2018-02-01 15:03:16 -0500

    set default socket to null

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,10 @@
 default["dbconf"]["database"] = "lamp_server"
 default["dbconf"]["db_username"] = "lamp_server"
 default["dbconf"]["db_password"] = "88888888"
-default["dbconf"]["socket"] = "/var/run/mysqld/mysqld.sock"
+default["dbconf"]["socket"] = "null"
 
 #db2
 default["dbconf2"]["database"] = "null"
 default["dbconf2"]["db_username"] = "null"
 default["dbconf2"]["db_password"] = "null"
-default["dbconf2"]["socket"] = "/var/run/mysqld/mysqld.sock"
+default["dbconf2"]["socket"] = "null"
