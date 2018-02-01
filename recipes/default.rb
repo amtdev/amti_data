899e8d82de161474cd1f279ee0af905d6cde3004
AMTdev Alien <craig@amtdev.com>
2018-01-31 20:55:41 -0500

    developed workaround for issue of IF EXISTS not available in MySQL 5.5 - developed drop/create user commands

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,33 +18,40 @@
 
 #Drop db
 execute "drop db if exists" do
-  command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e 'DROP DATABASE IF EXISTS #{node[:dbconf][:database]};'"
+  command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"DROP DATABASE IF EXISTS #{node[:dbconf][:database]};\""
+  action :run
 end
 
 # Create db
 execute "Create database" do
-  command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e 'CREATE DATABASE #{node[:dbconf][:database]} DEFAULT CHARACTER SET `utf8`;'"
+  command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"CREATE DATABASE #{node[:dbconf][:database]} DEFAULT CHARACTER SET utf8;\""
   action :run
 end
 
-#drop user
-#execute "drop user if exists" do
-#  command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"DROP USER IF EXISTS '#{node[:dbconf][:db_username]}'@'%';\""
-#end
+#drop user if user file exists
+user_created_file = "/home/vagrant/.#{node[:dbconf][:db_username]}_created"
+if File.exist?(user_created_file)
+    execute "drop user" do
+      command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"DROP USER '#{node[:dbconf][:db_username]}'@'%';\" && rm  /home/vagrant/.#{node[:dbconf][:db_username]}_created"
+      action :run
+    end
+end
 
 execute "Create database user" do
-    command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"CREATE USER IF NOT EXISTS '#{node[:dbconf][:db_username]}'@'%' IDENTIFIED BY '#{node[:dbconf][:db_password]}';\""
+    command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"CREATE USER '#{node[:dbconf][:db_username]}'@'%' IDENTIFIED BY '#{node[:dbconf][:db_password]}';\" && touch /home/vagrant/.#{node[:dbconf][:db_username]}_created"
+    creates  "/home/vagrant/.#{node[:dbconf][:db_username]}_created"
+    action :run
 end
 
-execute "Give root access from everywhere" do
-  command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '#{node[:mysql][:server_root_password]}' WITH GRANT OPTION;\""
-  action :run
-end
+#execute "Give root access from everywhere" do
+  #command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '#{node[:mysql][:server_root_password]}' WITH GRANT OPTION;\""
+  #action :run
+#end
 
-execute "Give #{node[:dbconf][:db_username]} access to #{node[:dbconf][:database]} from everywhere" do
-  command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"GRANT ALL PRIVILEGES ON #{node[:dbconf][:database]} .* TO '#{node[:dbconf][:db_username]}'@'%' WITH GRANT OPTION;\""
-  action :run
-end
+#execute "Give #{node[:dbconf][:db_username]} access to #{node[:dbconf][:database]} from everywhere" do
+  #command "mysql -uroot -p#{node[:mysql][:server_root_password]} -e \"GRANT ALL PRIVILEGES ON #{node[:dbconf][:database]} .* TO '#{node[:dbconf][:db_username]}'@'%' WITH GRANT OPTION;\""
+  #action :run
+#end
 
 if 'null' != node['dbconf']['socket']
     #socket = '-S "+node['dbconf']['socket']"
