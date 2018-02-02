7b8812bf56678f1cce22ff621bd9ce772fedbc3a
AMTdev Alien <craig@amtdev.com>
2018-02-01 21:09:40 -0500

    added db file folder default attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,11 @@ default["dbconf"]["database"] = "lamp_server"
 default["dbconf"]["db_username"] = "lamp_server"
 default["dbconf"]["db_password"] = "88888888"
 default["dbconf"]["socket"] = "null"
+default["dbconf"]["file_folder"] = "default"
 
 #db2
 default["dbconf2"]["database"] = "null"
 default["dbconf2"]["db_username"] = "null"
 default["dbconf2"]["db_password"] = "null"
 default["dbconf2"]["socket"] = "null"
+default["dbconf2"]["file_folder"] = "default"
